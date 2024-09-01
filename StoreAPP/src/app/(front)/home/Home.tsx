e1889ed6877934eba2b521b079734114d1201640
siwat jermwatthana <siwat.j@hiveground.com>
2024-09-01 20:38:59 +0700

    Feat: Add Animation and Banner components for front page
    - Add Animation component for fading in elements on scroll
    - Add Banner component for displaying a promotional banner on the front page

--- StoreAPP/src/app/(front)/home/Home.tsx
+++ StoreAPP/src/app/(front)/home/Home.tsx
@@ -1,26 +1,12 @@
-import { Typography, Button } from "@mui/material";
-import { AddCircleOutline as AddIcon } from "@mui/icons-material";
+'use client'
+
+import React from 'react'
+import Banner from '@/app/components/front/banner/Banner'
 
 const HomePage = () => {
   return (
     <>
-      {/* Typography */}
-      <Typography variant="h1">Next.JS MUI App</Typography>
-
-      {/* Button */}
-      <Button variant="contained" color="primary">
-        Click Me
-      </Button>
-
-      {/* Icons */}
-      <div>
-        <AddIcon color="primary" />
-        <AddIcon color="secondary" />
-        <AddIcon color="info" />
-        <AddIcon color="warning" />
-        <AddIcon color="error" />
-        <AddIcon color="success" />
-      </div>
+      <Banner />
     </>
   );
 };
