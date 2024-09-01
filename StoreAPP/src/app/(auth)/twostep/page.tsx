c299dc5cb573ebe494370f71a0b5367832794ecf
siwat jermwatthana <siwat.j@hiveground.com>
2024-09-01 22:05:01 +0700

    Feat: Add TwostepPage component for two-step verification
    - Implement TwostepPage component for two-step verification feature
    - Add layout and styling for the TwostepPage component
    - Include AuthTwoSteps form in the TwostepPage component

---
+++ StoreAPP/src/app/(auth)/twostep/page.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Metadata } from "next";
+import TwostepPage from "./Twostep";
+
+export const metadata: Metadata = {
+  title: "Two Step Verification",
+  description: "Two Step Verification page description",
+  keywords: ["Two Step Verification", "Next.js"],
+};
+
+type Props = {};
+
+export default function Twostep({}: Props) {
+  return <TwostepPage />;
+}
