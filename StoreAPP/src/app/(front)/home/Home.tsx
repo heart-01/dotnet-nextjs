37ed8476d968d0aac0d717bd7b83dd9889393f56
siwat jermwatthana <siwat.j@hiveground.com>
2024-09-01 20:43:46 +0700

    Feat: Add Frameworks component to home page
    - Add Frameworks component to display frameworks on the home page
    - Update Home component to include Frameworks component

--- StoreAPP/src/app/(front)/home/Home.tsx
+++ StoreAPP/src/app/(front)/home/Home.tsx
@@ -2,11 +2,13 @@
 
 import React from 'react'
 import Banner from '@/app/components/front/banner/Banner'
+import Frameworks from '@/app/components/front/frameworks/Frameworks';
 
 const HomePage = () => {
   return (
     <>
       <Banner />
+      <Frameworks />
     </>
   );
 };
