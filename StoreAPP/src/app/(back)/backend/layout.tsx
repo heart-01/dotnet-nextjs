6ca10049ff1d1b6899a6f9023e218a2213aa1478
siwat jermwatthana <siwat.j@hiveground.com>
2024-09-02 22:23:12 +0700

    Feat: Add UI Header in backend layout

--- StoreAPP/src/app/(back)/backend/layout.tsx
+++ StoreAPP/src/app/(back)/backend/layout.tsx
@@ -1,7 +1,8 @@
 "use client";
 
-import { styled } from "@mui/material";
+import { Box, Container, styled } from "@mui/material";
 import Sidebar from "@/app/components/back/sidebar/Sidebar";
+import Header from "@/app/components/back/header/Header";
 
 const MainWrapper = styled("div")(() => ({
   display: "flex",
@@ -9,10 +10,35 @@ const MainWrapper = styled("div")(() => ({
   width: "100%",
 }));
 
+const PageWrapper = styled("div")(() => ({
+  display: "flex",
+  flexGrow: 1,
+  paddingBottom: "60px",
+  flexDirection: "column",
+  zIndex: 1,
+  width: "100%",
+  backgroundColor: "transparent",
+}));
+
 const BackLayout = ({ children }: Readonly<{ children: React.ReactNode }>) => {
   return (
     <MainWrapper>
+      {/* Sidebar */}
       <Sidebar />
+
+      <PageWrapper>
+        {/* Header */}
+        <Header />
+
+        <Container
+          sx={{
+            maxWidth: "100%!important",
+          }}
+        >
+          {/* Page Content */}
+          <Box sx={{ minHeight: "calc(100vh - 170px)" }}>{children}</Box>
+        </Container>
+      </PageWrapper>
     </MainWrapper>
   );
 };
